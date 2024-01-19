6adf2ab3cae4fc2bf5fd65d458320e558cf27098
Rahul Sharma <42321121+rahulsharma4578@users.noreply.github.com>
2024-01-19 06:20:59 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail= "hitesh@google.com"
+var accountPassword="12345"
+let accountCity="Jaipur"
+
+accountEmail= "hc@hc.com"
+accountPassword= "212122"
+accountCity= "Bengaluru"
+
+/*
+-->please dont use var keyword as it has issues with the the block and the functional scope.
+-->when no type is mentioned out of let,const then javscript consider it as undefined. 
+
+*/
+
+console.table([accountEmail,accountPassword,accountCity])
